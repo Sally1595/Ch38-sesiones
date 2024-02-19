f1295dd2b605fbee1b7ce51a2307d98cae8f3ae6
Claudia Rosas <claudiar.alvarez95@gmail.com>
2024-02-19 13:00:17 -0600

    Saludar a persona

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Hola Ch38");
+
+// Función que enie un saludo con alert 
+function enviarSaludi(){
+    alert('¡Hola Ch38!');
+}
+
+/**
+ * Pedir el nombre de la persona usando promt()
+ * Mostrar un saludo y nombre de la persona con alert()
+ */
+function enviarSaludoAPersona(){
+prompt('Pon tu nombre');
+alert('Hola ')
+}
